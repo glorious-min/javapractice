22c478c4aa60803ef5788678bced67b1877b0726
glorious-min <70811550+glorious-min@users.noreply.github.com>
2020-12-04 15:09:59 +0900

    20200902

---
+++ 20200902/src/BreakTest.java
@@ -0,0 +1,18 @@
+
+public class BreakTest {
+
+	public static void main(String[] args) {
+		for (int i = 1 ; i <100; i++    ) {
+			
+			System.out.println("i="+i);
+			if (i==50) break;//
+		}
+		
+		while (true) if (1==1){
+			break;
+			
+		}
+	}
+	
+
+}

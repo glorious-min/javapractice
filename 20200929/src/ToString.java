9ccb2e6481ed3e514a28a458b0aeb6724d348f12
glorious-min <70811550+glorious-min@users.noreply.github.com>
2020-12-04 23:57:51 +0900

    20200930

--- 20200929/src/ToString.java
+++
@@ -1,8 +0,0 @@
-
-public class ToString {
-
-		 public static void main (String[] args) {
-			 Member member = new Member("홍길동",25,"서울시");
-			 System.out.println(member);
-		 }
-}
--- 20200929/src/ToString3.java
+++
@@ -1,9 +0,0 @@
-
-public class ToString3 {
-
-	public static void main(String[] args) {
-		Member3 member = new Member3("이경민",70, "캐나다");// 여기에서 세개의 파라미터 값 넣으라는 안내 바로 나옴 !!
-		System.out.println(member.toString());
-	}
-
-}

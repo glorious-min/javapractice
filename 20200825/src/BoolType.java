7973538e766a2117eabf72831253c7bbcb3ad902
glorious-min <70811550+glorious-min@users.noreply.github.com>
2020-12-04 13:50:17 +0900

    20200825

---
+++ 20200825/src/BoolType.java
@@ -0,0 +1,10 @@
+
+public class BoolType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    boolean b = true;
+    System.out.println(b);
+	}
+
+}
---
+++ 20200825/src/CharacterType.java
@@ -0,0 +1,12 @@
+
+public class CharacterType {
+
+	public static void main(String[] args) {
+		char c1 = 'a';
+		char c2 = '\u0061';
+        
+		System.out.println(c1);
+		System.out.println(c2);
+	}
+
+}

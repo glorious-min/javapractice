5cd91572be4ef75c79ed701371682dc159f7c5a5
glorious-min <70811550+glorious-min@users.noreply.github.com>
2020-12-04 16:22:49 +0900

    20200911

---
+++ 20200911/src/arrayquiz1.java
@@ -0,0 +1,32 @@
+
+public class arrayquiz1 {
+
+	public static void main(String[] args) {
+		int[] arr = { 5, 3, 8, 1 };
+		int i,j,temp;
+       /*System.out.println("정렬전");
+        for(i=0;i<arr.length;i++) {
+        	System.out.println("\t"+arr[i]);
+        }
+        System.out.println();*/
+        
+		
+        for(i=0;i<arr.length;i++) {
+        	for(j=0;j<arr.length-1-i;j++) {
+        		//이미 끝난 뒷부분은 하든 안하든 상관 없으니까 이렇게 하기 !!
+                //?-i를 하던 안하던 값이 같게 나온다 ?
+        		if(arr[j]>arr[j+1]) {
+        			temp=arr[j+1];
+        			arr[j+1] = arr[j];
+        			arr[j] = temp;
+        		}
+        	}      	
+        }
+        System.out.println("정렬후");
+        for(i=0;i<arr.length;i++) {
+        	
+        	System.out.println(arr[i]);
+        }
+	}
+}
+//마지막 페이스가 실행되지 않은채로 결과가 출력됨 
---
+++ 20200911/src/arrayquiz2.java
@@ -0,0 +1,22 @@
+
+public class arrayquiz2 {
+
+	public static void main(String[] args) {
+		int arr []= {3,4,6,2,8,1};
+		int i;
+		int temp;
+		for(i=1 ; i < arr.length ;i++) {
+			for(int j=i;j>0;j--) {
+				if(arr[j-1]<arr[j]) {
+					temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		for(i=0;i<arr.length;i++) {
+		System.out.println(arr[i]);
+	}}
+
+}

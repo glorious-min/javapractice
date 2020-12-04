a657a1db22b2051a5199b2f010caaaee49c3a07f
glorious-min <70811550+glorious-min@users.noreply.github.com>
2020-12-04 15:51:01 +0900

    20200907

---
+++ 20200907/src/Practice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//배열의 크기를 직접입력하라 
+public class Practice {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.print("배열의 크기를 입력하세요 ");
+		int length = sc.nextInt();
+		int[] intArr = new int[ length];
+		
+		
+	   for (int i=0; i < intArr.length;i++) {
+		   System.out.println(intArr[i]);
+	   }
+
+	}
+
+}
